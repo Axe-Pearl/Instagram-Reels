4e6b613bcba3d685fcfa73696987af57c720f4ee
Axe-Pearl <mycoursesbattletour@gmail.com>
2021-11-17 23:59:10 +0530

    red bug

--- src/VideoHeader.js
+++ src/VideoHeader.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "./VideoHeader.css";
-// import AddReactionIcon from '@mui/icons-material/AddReaction';
-// import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 // import CameraAltIcon from '@mui/icons-material/CameraAlt';
+// import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import AdbIcon from '@material-ui/icons/Adb';
 
 function VideoHeader(){
    return(
       <div className="videoHeader">
-         {/* <AddReactionIcon /> */}
-      {/* <ArrowBackIosIcon /> */}
+      
       <h3>Reels</h3>
-      {/* <CameraAltIcon /> */}
+      <AdbIcon />
       </div>
    );
 }
